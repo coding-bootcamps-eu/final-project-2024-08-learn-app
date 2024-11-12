259b810297174a30b2f277836816874169e982fe
Steffen Schanze <steffenjoachim.schanze@gmail.com>
2024-11-12 15:17:09 +0100

    TestView added

--- src/components/IndexCard.vue
+++ src/components/IndexCard.vue
@@ -21,10 +21,9 @@ export default {
 div {
   border: solid 2px var(--clr-green-dark);
   padding: 2rem;
-  margin-inline: auto;
-  width: 90%;
-  max-width: 350px;
-  height: 200px;
+  width: 100%;
+  max-width: 550px;
+  aspect-ratio:  10 / 7;
   border-radius: 5px;
   background-color: var(--clr-white);
   display: flex;
@@ -45,26 +44,15 @@ div {
   color: var(--clr-white);
 }
 
-/* Hier die Karte nochmal zweimal zu verkleinern scheint mir unnötig */
-@media (max-width: 480px) {
-}
-
-@media (min-width: 481px) and (max-width: 768px) {
-}
-
-@media (min-width: 769px) and (max-width: 1279px) {
+@media (min-width: 769px) {
   div {
-    width: 80%;
-    max-width: 400px;
-    height: 220px;
     padding: 1.5rem;
+    aspect-ratio: 10 / 5;
   }
 }
 
 @media (min-width: 1280px) {
   div {
-    max-width: 500px;
-    height: 300px;
     padding: 2rem;
   }
 }
--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -1,39 +1,53 @@
 <template>
-    <button :class="colors">{{ text }}</button>
+  <button :class="colors">{{ text }}</button>
 </template>
 
 <script>
 export default {
-    props: {
-        text: String,
-        colors: {
-            type: String,
-            default: "green",
-        }
-    }
-};
+  props: {
+    text: String,
+    colors: {
+      type: String,
+      default: 'green',
+    },
+  },
+}
 </script>
 
 <style scoped>
 button {
-    padding: .75rem 2.25rem;
-    border-radius: .25rem;
-    border: none;
-    color: var(--clr-white);
+  padding: 0.25rem 0.75rem;
+  border-radius: 0.25rem;
+  border: none;
+  color: var(--clr-white);
+  font-size: 1.1rem;
+  font-weight: bold;
+}
+
+.green {
+  background-color: var(--clr-green-dark);
+  color: var(--clr-white);
+}
+
+.lightgreen {
+  background-color: var(--clr-green-light);
+}
+
+.orange {
+  background-color: var(--clr-orange);
+}
+
+@media (min-width: 480px) {
+  button {
     font-size: 1.25rem;
-    font-weight: bold;
+    padding: 0.5rem 1.5rem;
   }
 
-  .green {
-    background-color: var(--clr-green-dark);
-    color: var(--clr-white);
+  @media (min-width: 768px) {
+    button {
+      font-size: 1.5rem;
+      padding: 0.75rem 2.25rem;
+    }
   }
-
-  .lightgreen {
-    background-color: var(--clr-green-light);
-  }
-
-  .orange {
-    background-color: var(--clr-orange);
-  }
+}
 </style>
