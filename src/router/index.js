a21b7e6edf6c7e93dfcfd757031b358514dc7369
Nossa Schaefer <nossa.schaefer@gmail.com>
2024-11-13 14:26:39 +0100

    Path für ProfileView gesetzt

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import TestView from '@/views/TestView.vue'
 import CategoriesView from '@/views/CategoriesView.vue'
 import LoginView from '@/views/LoginView.vue'
 import LearnView from '@/views/LearnView.vue'
+import ProfileView from '@/views/ProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -33,6 +34,14 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+      meta: {
+        isPublic: true,
+      },
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView,
       meta: {
         isPublic: true,
       },
