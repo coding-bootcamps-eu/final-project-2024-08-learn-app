f7c685bdb93ac1276df8eeefac89b9a2ed13810f
Andy Huschka <andy_huschka@web.de>
2024-11-15 10:54:26 +0100

    Mainheader hotfix : add more padding

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -94,7 +94,7 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 1rem 3rem;
+  padding: 1rem 4.5rem;
   background-color: var(--clr-green-dark);
   color: #fff;
 }
