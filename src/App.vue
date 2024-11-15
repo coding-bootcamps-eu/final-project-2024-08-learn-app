c947444e5af7772d1ff176293fc14e034a7a720b
Andy Huschka <andy_huschka@web.de>
2024-11-15 11:21:35 +0100

    i create a simple footer and integrated it into the App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <MainHeader />
   <RouterView />
+  <AppFooter />
 </template>
 
 <script>
 import MainHeader from './components/MainHeader.vue'
+import AppFooter from './components/Footer.vue'
 
 export default {
   components: {
     MainHeader,
+    AppFooter,
   },
 }
 </script>
